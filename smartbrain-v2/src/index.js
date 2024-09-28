dcba526f7bea2a61abcce2ae05486e35fcf58c26
Qassim Osseni <qassimosseni@gmail.com>
2024-09-28 22:42:03 +0200

    ajout projet final

--- smartbrain-v2/src/index.js
+++ smartbrain-v2/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-// import reportWebVitals from './reportWebVitals';
+// import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
-registerServiceWorker();
+const root = createRoot(document.getElementById('root'));
+
+root.render(<App />)
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// registerServiceWorker();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
